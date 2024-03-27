c73d705c5d00d9330b3f3fe1ef80343baba287cf
ala-kad <kaddechiala@gmail.com>
2024-03-27 10:16:44 +0100

    fixing getOneUser endpoint

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,17 +5,15 @@ const getAllUsers = async (req, res)  => {
         const users = await User.find();
         res.status(200).send(users);
     } catch (error) {
-        console.log(error);
         res.status(500).send(error);
     }
 }
 
 const getOneUser = async (req, res)  => { 
     try {
-        const user = await User.find();
-        res.status(200).send(users);
+        const user = await User.findById(req.params.id);
+        res.status(200).send(user);
     } catch (error) {
-        console.log(error);
         res.status(500).send(error);
     }
 }
@@ -23,21 +21,20 @@ const getOneUser = async (req, res)  => {
 const updateUser = async (req, res) => { 
     try{ 
         await User.findByIdAndUpdate(req.params.id, req.body);
-        res.status(200).send(`User updated`)
+        res.status(200).send(`User updated`);
     }catch(error){
-        res.status(400).send(error.message)
+        res.status(400).send(error);
     }
 }
 
 const deleteUser = async (req, res) => { 
     try {
         const user = await User.findById(req.params.id);
-        console.log(`USER = ${user}`);
         if(!user) { return res.status(404).send(`User not found`) };
         await user.deleteOne();
         return res.status(200).send(`User deleted`);
     }catch(err) { 
-        return res.status(500).send(err)
+        return res.status(500).send(err);
     }
 }
 
