9951f558d409909f4808cdf9c942bd2e63a78fff
ala-kad <kaddechiala@gmail.com>
2024-03-27 16:03:33 +0100

    adpating user schema, roles discriminator

--- models/user.js
+++ models/user.js
@@ -11,11 +11,36 @@ const userSchema = new mongoose.Schema({
     password: String,
     role: {
         type: String,
-        enum: ['admin', 'candidate', 'recruiter', 'guest']
-    }
+        enum: ['admin', 'candidate', 'recruiter', 'guest'], 
+        required: true
+    },
 },{ versionKey: false});
+
+const adminSchema = new mongoose.Schema({ 
+   
+})
+const candidateSchema = new mongoose.Schema({ 
+    firstname: String, 
+    lastname: String,
+    adress: String,
+    phone: Number,
+    applications: String // To-Do change to obect ID Many to Many (One job have 1..n candidates, 1 Candidate have 1..n applications)
+}, {discriminatorKey: 'role'});
+
+const recruiterSchema = new mongoose.Schema({ 
+    firstname: String, 
+    lastname: String,
+    adress: String,
+    phone: Number,
+    jobs: String // To-Do change to obect ID One User to Many Jobs (One job have 1 Recruiter(author), 1 Recruiter writes 1..n jobs)
+}, {discriminatorKey: 'role'});
+
 
 // userSchema.plugin(passportLocalMongoose)
 const User = mongoose.model('User', userSchema);
 
-module.exports = User;
+const Admin = User.discriminator('admin',userSchema );
+const Candidate = User.discriminator('candidate', candidateSchema);
+const Recruiter = User.discriminator('recruiter', recruiterSchema);
+
+module.exports = { User, Admin, Candidate, Recruiter };
