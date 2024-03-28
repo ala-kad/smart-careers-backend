1b1f67d1c1c9ef4861ef1b73122baf3d61094d18
ala-kad <kaddechiala@gmail.com>
2024-03-28 16:48:34 +0100

    adapting USer model Schemas

--- models/user.js
+++ models/user.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 // const passportLocalMongoose = require('passport-local-mongoose');
 
+const  Permissions = ['create', 'read', 'update', 'delete', 'all', 'none'];
+const Roles = ['admin', 'recruiter', 'candidate'];
+   
 const userSchema = new mongoose.Schema({
     email: { 
         type: String,
@@ -8,29 +11,47 @@ const userSchema = new mongoose.Schema({
         unique: true
     },
     username: String,
-    password: String,
-    role: {
+    password: {
         type: String,
-        enum: ['admin', 'candidate', 'recruiter', 'guest'], 
         required: true
     },
+    role: {
+        type: Roles,
+    },
+    permissions: [String],
 },{ versionKey: false});
 
+/**
+ * Admin Schema
+ */
 const adminSchema = new mongoose.Schema({ 
-   
-})
+   isAdmin: {
+    type: Boolean,
+    default: true
+    }
+}, {discriminatorKey: 'role'});
+
+/**
+ * Candidate Schema
+ */
 const candidateSchema = new mongoose.Schema({ 
     firstname: String, 
     lastname: String,
     adress: String,
     phone: Number,
-    applications: String // To-Do change to obect ID Many to Many (One job have 1..n candidates, 1 Candidate have 1..n applications)
+    photo: String, 
+    cvFilename: String,
+    skills: [String],
+    experienceYears: Number,
+    applications: [String] // To-Do change to obect ID Many to Many (One job have 1..n candidates, 1 Candidate have 1..n applications)
 }, {discriminatorKey: 'role'});
 
+/**
+ * Recruiter Schema
+ */
 const recruiterSchema = new mongoose.Schema({ 
     firstname: String, 
     lastname: String,
-    adress: String,
     phone: Number,
     jobs: String // To-Do change to obect ID One User to Many Jobs (One job have 1 Recruiter(author), 1 Recruiter writes 1..n jobs)
 }, {discriminatorKey: 'role'});
@@ -39,7 +60,7 @@ const recruiterSchema = new mongoose.Schema({
 // userSchema.plugin(passportLocalMongoose)
 const User = mongoose.model('User', userSchema);
 
-const Admin = User.discriminator('admin',userSchema );
+const Admin = User.discriminator('admin', adminSchema );
 const Candidate = User.discriminator('candidate', candidateSchema);
 const Recruiter = User.discriminator('recruiter', recruiterSchema);
 
