c8a8414708918b9f2e115af6f6593e3f9b0716a7
ala-kad <kaddechiala@gmail.com>
2024-04-21 22:00:54 +0100

    jobs model schema

--- models/job.js
+++ models/job.js
@@ -2,9 +2,19 @@ const mongoose = require('mongoose');
 
 const jobSchema = new mongoose.Schema({
     title: String, 
+    company: String,
+    skills: [String],
+    requiredExperience: Number,
     description: String, 
-    publishedOn: Date,
-})
+    location: {
+       type: String,
+       enum: ['onsite', 'hybrid', 'remote']
+    },
+    publishedOn: {
+        type: Date,
+        default: Date.now()
+    },
+}, { versionKey: false });
 
 const Job = mongoose.model('Job', jobSchema);
 
