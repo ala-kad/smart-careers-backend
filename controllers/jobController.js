3d5a5d4d898c4d7714bc49cf4c5c8859e0d87968
ala-kad <kaddechiala@gmail.com>
2024-04-24 16:13:17 +0100

    job listing based on status endpoint

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -11,8 +11,25 @@ const createJob = async (req, res) => {
 
 const getAllJobs = async (req, res) => { 
     try{
-        const jobs = await Job.find();
-        res.status(201).json(jobs);
+        console.log('Req Params', req.params.status);
+        console.log('Req Query', req.query.status);
+        switch(req.query.status) { 
+            case 'Draft': 
+                console.log('Draft');
+                const draftJobs = await Job.find({ status: 'Draft'});
+                res.status(200).json(draftJobs);
+            break;
+            case 'Published':
+                console.log('Published');
+                const publishedJobs = await Job.find({ status: 'Published'});
+                res.status(200).json(publishedJobs);
+            break;
+            default: 
+                console.log('Default');
+                const jobs = await Job.find();
+                res.status(200).json(jobs);
+            break;
+        }
     } catch (error) {
         res.status(500).json(error);
     }
@@ -53,4 +70,20 @@ const deleteJob = async (req, res) => {
     }
 }
 
-module.exports = { createJob, getAllJobs, getOneJob, updateJob, deleteJob } ; 
+const publishJob = async(req, res) => { 
+    try{ 
+        const job = await Job.findById(req.params.id);
+        job.status = 'Published';
+        await job.save()
+        .then(
+            updatedJob => {
+                console.log(updatedJob);
+                res.status(201).json('Job status updated');
+        })
+
+    }catch(error) { 
+        res.status(500).json(error)
+    }
+}
+
+module.exports = { createJob, getAllJobs, getOneJob, updateJob, deleteJob, publishJob } ; 
