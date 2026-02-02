4084619ae8e940c99aa6573c4e773e57cdbc6ce8
ala-kad <kaddechiala@gmail.com>
2026-02-02 23:19:15 +0100

    refactor: streamline admin account initialization and role seeding process

--- config/init.js
+++ config/init.js
@@ -1,35 +1,46 @@
-var User = require('../models/user');
-var bcrypt = require('bcrypt');
-var connect = require('../config/db');
+const User = require('../models/user');
+const Role = require('../models/role');
+const rolesSeed = require('../models/roles');
+const bcrypt = require('bcryptjs');
+const connect = require('../config/db');
 
-const initAdminAccount = async() => { 
-    try{ 
-        await connect();
-        let admin = await User.findOne({ role: 'admin'});
-        if(admin) { 
-            console.log('Admin exists') 
-            // res.status(400).send('Admin exists');
-        }
-        else{
-            password = process.env.ADMIN_PSWD;
-            bcrypt.hash(password, 10, async (err, hash) => {
-                admin = await User.create({
-                    email: 'kaddechiala@gmail.com',
-                    username: 'alakad',
-                    password: hash,
-                    role: ['admin']
-                });
-                await admin.save();
-               console.log('Admin created');
-            //    res.status(200).send('Admin created');
-            })
-        }
-    }catch (err) { 
-        console.log(err.message);
-        // res.status(500).send('Error in creating admin');
-    }
-}
+const initAdminAccount = async () => {
+    // Ensure DB connection and seed roles idempotently
+    await connect();
 
-initAdminAccount();
+    await Promise.all(
+        rolesSeed.map(r =>
+            Role.updateOne(
+                { name: r.name },
+                { $setOnInsert: { name: r.name } },
+                { upsert: true }
+            )
+        )
+    );
 
-module.exports = { initAdminAccount } ; 
+    const adminRole = await Role.findOne({ name: 'admin' });
+    if (!adminRole) throw new Error('Admin role not found after seeding');
+
+    const admin = await User.findOne({ role: { $in: [adminRole._id] } });
+    if (admin) return { status: 'admin_exists' };
+
+    const password = process.env.ADMIN_PSWD;
+    if (!password) throw new Error('ADMIN_PSWD is not configured');
+
+    const hash = await bcrypt.hash(password, 10);
+    const created = await User.create({
+        email: 'kaddechiala@gmail.com',
+        username: 'alakad',
+        password: hash,
+        role: [adminRole._id]
+    });
+
+    return { status: 'admin_created', id: created._id };
+};
+
+initAdminAccount().catch(err => {
+    // Emit a process warning so upstream process managers or logs can capture it
+    process.emitWarning(`initAdminAccount failed: ${err.message}`);
+});
+
+module.exports = { initAdminAccount }; 
